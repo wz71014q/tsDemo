ca2fdc283f233aca035e48b79c56c625f4673e44
zhi qiang <zhiqiang.wang@yff.com>
2019-10-30 17:16:36 +0800

    feat: swiper touch event

--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,8 @@
 module.exports = {
     // 一行最多 100 字符
     printWidth: 100,
-    // 使用 4 个空格缩进
-    tabWidth: 2,
+    // 使用 2 个空格缩进
+    tabWidth: 1,
     // 不使用缩进符，而使用空格
     useTabs: false,
     // 行尾需要有分号
@@ -15,7 +15,7 @@ module.exports = {
     // jsx 不使用单引号，而使用双引号
     jsxSingleQuote: false,
     // 末尾不需要逗号
-    trailingComma: 'none',
+    trailingComma: 'es5',
     // 大括号内的首尾需要空格
     bracketSpacing: true,
     // jsx 标签的反尖括号需要换行
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,85 +1,118 @@
 <template>
-    <div class="picker">
-        <div class="change" @click="changeContent">click</div>
-        <div class="picker-item-wrapper" :style="wrapperStyle" @touchstart="swiperStart">
-            <section
-                class="picker-item"
-                :key="'section_' + index"
-                v-for="(item, index) in dataList"
-                :style="setItemStyle(index, dataList.length)"
-            >
-                {{ item.content }}
-            </section>
-        </div>
+  <div class="picker">
+    <div class="change" @click="changeContent">click</div>
+    <div class="picker-item-wrapper" :style="wrapperStyle" ref="pickerWrapper">
+      <section
+        class="picker-item"
+        :key="'section_' + index"
+        v-for="(item, index) in dataList"
+        :style="setItemStyle(index, dataList.length)"
+      >
+        {{ item.content }}
+      </section>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
-    name: 'Picker'
+  name: 'Picker',
 })
 export default class Picker extends Vue {
-    private restore = true;
-    private swiper = {
-        radio: 100
-    };
-    private dataList: object[] = [
-        {
-            content: '1',
-            align: 'center'
-        },
-        {
-            content: '2',
-            align: 'center'
-        },
-        {
-            content: '3',
-            align: 'center'
-        },
-        {
-            content: '4',
-            align: 'center'
-        },
-        {
-            content: '5',
-            align: 'center'
-        },
-        {
-            content: '6',
-            align: 'center'
-        },
-        {
-            content: '7',
-            align: 'center'
-        },
+  private restore = true;
+  private swiper = {
+    radio: 100,
+    startPoint: [0, 0],
+    movingPoint: [0, 0],
+    endPoint: [0, 0],
+  };
+  private dataList: object[] = [
+    {
+      content: '1',
+      align: 'center',
+    },
+    {
+      content: '2',
+      align: 'center',
+    },
+    {
+      content: '3',
+      align: 'center',
+    },
+    {
+      content: '4',
+      align: 'center',
+    },
+    {
+      content: '5',
+      align: 'center',
+    },
+    {
+      content: '6',
+      align: 'center',
+    },
+    {
+      content: '7',
+      align: 'center',
+    },
+  ];
+  get wrapperStyle(): string {
+    const res = this.restore ? 'transform: rotateY(0deg)' : 'transform: rotateY(30deg)';
+    return res;
+  }
+  private mounted() {
+    const selfPickerWrapper: any = this.$refs.pickerWrapper;
+    selfPickerWrapper.addEventListener('touchstart', this.swiperStart, {
+      passive: false,
+      capture: false,
+    });
+  }
+  private setItemStyle(index: number, length: number): string {
+    let res;
+    if (this.restore === true) {
+      res = 'transform: rotateX(0deg) translateZ(0px)';
+    } else {
+      res = `transform: rotateX(${(360 / length) * index}deg) translateZ(${this.swiper.radio}px)`;
+    }
+    return res;
+  }
+  private changeContent(): void {
+    this.restore = !this.restore;
+  }
+  private swiperStart(event: any) {
+    const myEvent = event || window.event;
+    const selfPickerWrapper: any = this.$refs.pickerWrapper;
+    this.swiper.startPoint = [
+      myEvent.clientX || myEvent.targetTouches[0].pageX,
+      myEvent.clientY || myEvent.targetTouches[0].pageY,
     ];
-    get wrapperStyle(): string {
-        const res = this.restore ? 'transform: rotateY(0deg)' : 'transform: rotateY(50deg)';
-        return res;
-    }
-    private setItemStyle(index: number, length: number): string {
-        let res;
-        if (this.restore === true) {
-            res = 'transform: rotateX(0deg) translateZ(0px)';
-        } else {
-            res = `transform: rotateX(${(360 / length) * index}deg) translateZ(${
-                this.swiper.radio
-            }px)`;
-        }
-        return res;
-    }
-    private changeContent(): void {
-        this.restore = !this.restore;
-    }
-    private swiperStart(event: any) {
-        const myEvent = event || window.event;
-        const startPoint = [
-            myEvent.clientX || myEvent.targetTouches[0].pageX,
-            myEvent.clientY || myEvent.targetTouches[0].pageY,
-        ];
-    }
+    selfPickerWrapper.addEventListener('touchmove', this.swiperMoving, {
+      passive: false,
+      capture: false,
+    });
+    selfPickerWrapper.addEventListener('touchend', this.swiperEnd, {
+      passive: false,
+      capture: false,
+    });
+  }
+  private swiperMoving(event: any) {
+    const myEvent = event || window.event;
+    this.swiper.movingPoint = [
+      myEvent.clientX || myEvent.targetTouches[0].pageX,
+      myEvent.clientY || myEvent.targetTouches[0].pageY,
+    ];
+  }
+  private swiperEnd(event: any) {
+    const myEvent = event || window.event;
+    this.swiper.endPoint = [
+      myEvent.clientX || myEvent.changedTouches[0].pageX,
+      myEvent.clientY || myEvent.changedTouches[0].pageY,
+    ];
+    console.log('X-axis', this.swiper.endPoint[1] - this.swiper.startPoint[1]);
+    console.log('Y-axis', this.swiper.endPoint[0] - this.swiper.startPoint[0]);
+  }
 }
 </script>
 
